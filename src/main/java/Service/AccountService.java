389bbef27da483f9cfd7b799329557dd44944ff6
Jon Sundin <sundinjon@gmail.com>
2024-01-25 23:47:26 +0000

    account service

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,20 @@
+package Service;
+
+import Model.Account;
+import DAO.AccountDAO;
+
+public class AccountService {
+    AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    public Account addAccount(Account account){
+        return accountDAO.insertAccount(account);
+    }
+}
