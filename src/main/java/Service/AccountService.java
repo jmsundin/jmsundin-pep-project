a405874ce887331c2f49b0d4eccad476eebfcdc7
Jon Sundin <sundinjon@gmail.com>
2024-01-28 08:35:38 +0000

    service methods for accounts

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -3,18 +3,45 @@ package Service;
 import Model.Account;
 import DAO.AccountDAO;
 
+import java.util.List;
+
 public class AccountService {
     AccountDAO accountDAO;
 
-    public AccountService(){
+    public AccountService() {
         accountDAO = new AccountDAO();
     }
 
-    public AccountService(AccountDAO accountDAO){
+    public AccountService(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
     }
 
-    public Account addAccount(Account account){
-        return accountDAO.insertAccount(account);
+    public Account addAccount(Account account) {
+        String username = account.getUsername();
+        String password = account.getPassword();
+        return accountDAO.insertAccount(username, password);
+    }
+
+    public boolean isValidCredentials(String username, String password) {
+        if (accountIsRegistered(username)) return false;
+        else if (username.isEmpty()) return false;
+        if (password.length() < 4) return false;
+        return true;
+    }
+
+    public boolean accountIsRegistered(String username) {
+        if (accountDAO.findAccountByUsername(username) != null) return true;
+        return false;
+    }
+
+    public Account login(String username, String password) {
+        Account registeredAccount = accountDAO.findAccountByUsername(username);
+        if (registeredAccount == null) return null;
+        if (!registeredAccount.getPassword().equals(password)) return null;
+        return registeredAccount;
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountDAO.getAllAccounts();
     }
 }
