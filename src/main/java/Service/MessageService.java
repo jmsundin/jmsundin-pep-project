e17a19eba7fd85700b6ee654dda737ec8ef84c03
Jon Sundin <sundinjon@gmail.com>
2024-01-30 01:50:54 +0000

    delete message by id service method

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -42,5 +42,9 @@ public class MessageService {
     public Message getMessageById(int message_id) {
         return messageDAO.getMessageById(message_id);
     }
+
+    public Message deleteMessageById(int message_id) {
+        return messageDAO.deleteMessageById(message_id);
+    }
     
 }
